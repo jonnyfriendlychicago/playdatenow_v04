f64ab02c578b622b17428971fccdf1206434e23c
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-09-08 09:38:03 -0500

    updated app.java file to enable put-to-web

--- src/main/java/com/jonfriend/playdatenow_v04/PlaydatenowV04Application.java
+++ src/main/java/com/jonfriend/playdatenow_v04/PlaydatenowV04Application.java
@@ -1,7 +1,11 @@
 package com.jonfriend.playdatenow_v04;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PlaydatenowV04Application {
@@ -9,5 +13,17 @@ public class PlaydatenowV04Application {
 	public static void main(String[] args) {
 		SpringApplication.run(PlaydatenowV04Application.class, args);
 	}
-
+	@Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setPort(9090);
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setScheme("http");
+       ((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+    tomcat.addAdditionalTomcatConnectors(ajpConnector);
+    return tomcat;
+    }
+// end of class
 }
